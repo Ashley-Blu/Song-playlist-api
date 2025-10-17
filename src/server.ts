a4177b852e898a8f5aa474013fc1af0473635f60
Ashley-Blu <matsekolengashley28@hmail.com>
2025-10-17 07:58:56 +0200

    Final commit

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
-import http, {IncomingMessage, ServerResponse} from 'http';
+import http, { IncomingMessage, ServerResponse } from "http";
+import { songsRoute } from "./routes/Songs";
 
 const PORT = 3000;
 
 const requestListener = (req: IncomingMessage, res: ServerResponse) => {
-    res.writeHead(200, {"content-type": "application/json"});
-    res.end(JSON.stringify({ message: "Hellow world!"}));
+
+  if (req.url?.startsWith("/songs")) {
+    songsRoute(req, res);
+  } else {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "Hellow world!" }));
+  }
 };
 
-const server = http.createServer(requestListener)
+const server = http.createServer(requestListener);
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
